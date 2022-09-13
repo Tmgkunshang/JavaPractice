0512963349c7b918b5ee88cc89a905367fdc8877
K_lama1999 <61765847+klama1999@users.noreply.github.com>
2022-09-13 14:03:32 +0930

    My fourth commit

--- JavaPractice/src/javaPractice.java
+++ JavaPractice/src/javaPractice.java
@@ -7,6 +7,7 @@ public class javaPractice {
 		System.out.println("This is my first commit in Java");
 		System.out.println("This is my second commit message");
 		System.out.println("This is my third commit");
+		System.out.println("This is my fourth commit");
 		
 	}
 }
