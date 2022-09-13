e64b7bad6332fca419b6e0efcde6e33e57e82427
K_lama1999 <61765847+klama1999@users.noreply.github.com>
2022-09-13 13:42:26 +0930

    first commit! uploading files

---
+++ JavaPractice/src/javaPractice.java
@@ -0,0 +1,9 @@
+
+public class javaPractice {
+	public static void main(String[] args) {
+		commitMsg();
+	}
+	public static void commitMsg() {
+		System.out.println("This is my first commit in Java");
+	}
+}
