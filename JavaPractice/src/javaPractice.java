0eef4633cdc4050054fe7932d04df5cd65f9fc44
K_lama1999 <61765847+klama1999@users.noreply.github.com>
2022-09-13 13:58:32 +0930

    This is my third commit

--- JavaPractice/src/javaPractice.java
+++ JavaPractice/src/javaPractice.java
@@ -6,6 +6,7 @@ public class javaPractice {
 	public static void commitMsg() {
 		System.out.println("This is my first commit in Java");
 		System.out.println("This is my second commit message");
+		System.out.println("This is my third commit");
 		
 	}
 }
